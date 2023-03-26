27217fd6db19d7784db7b01d6d8bb483a20b7992
melon <ortizfranco48@gmail.com>
2023-03-26 07:43:45 -0300

    error rendering latest blogs

--- blog/components/Latest.jsx
+++ blog/components/Latest.jsx
@@ -1,11 +1,57 @@
-import React from "react";
+import { GraphQLClient, gql } from "graphql-request";
+import Image from "next/image"; // for covers and thumbnails
+import Link from "next/link";
 
-const Latest = () => {
+const hygraph = new GraphQLClient(
+  "https://api-sa-east-1.hygraph.com/v2/clf4izifj4o1a01t7f0c3e6m0/master"
+);
+
+const QUERY = gql`
+  {
+    posts {
+      id
+      title
+      description
+      slug
+      coverImage {
+        url
+      }
+      author {
+        name
+        photo {
+          url
+        }
+      }
+      categories {
+        name
+        slug
+      }
+    }
+  }
+`;
+
+export async function getStaticProps() {
+  const { posts } = await hygraph.request(QUERY);
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+const Latest = ({ posts }) => {
   return (
     <div>
       <div className="grid grid-cols-2 items-center h-96">
         <h2 text-4xl>Latest Articles</h2>
-        <p>List of Blog Posts go Here!</p>
+        {posts?.slice(0, 2).map(({ id, title, slug, coverImage }) => (
+          <div key={id}>
+            <h2>
+              <Link href={`/post/${slug}`}>{title}</Link>
+            </h2>
+            <Image src={coverImage.url} width={150} height={150} />
+          </div>
+        ))}
       </div>
     </div>
   );
--- blog/components/Projects.jsx
+++ blog/components/Projects.jsx
@@ -1,11 +1,54 @@
-import React from "react";
+import Navbar from "@/components/Navbar";
+import { GraphQLClient, gql } from "graphql-request";
+import Image from "next/image"; // for covers and thumbnails
+import Link from "next/link";
+
+const hygraph = new GraphQLClient(
+  "https://api-sa-east-1.hygraph.com/v2/clf4izifj4o1a01t7f0c3e6m0/master"
+);
+
+const QUERY = gql`
+  {
+    posts {
+      title
+      description
+      slug
+      coverImage {
+        url
+      }
+      content {
+        html
+      }
+      author {
+        name
+        photo {
+          url
+        }
+        bio
+      }
+      categories {
+        name
+        slug
+      }
+    }
+  }
+`;
 
 const Projects = () => {
   return (
     <div>
       <div className="grid grid-cols-2 items-center h-96">
         <h2 text-4xl>Projects</h2>
-        <p>List of Projects go Here!</p>
+        {/* <div className="grid grid-cols-3">
+          {posts.slice(0, 3).map(({ title, slug, coverImage }) => (
+            <div key={slug}>
+              <h2>
+                <Link href={`/post/${slug}`}>{title}</Link>
+              </h2>
+              <Image src={coverImage.url} width={150} height={150} />
+            </div>
+          ))}
+        </div> */}
       </div>
     </div>
   );
